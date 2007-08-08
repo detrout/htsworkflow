730f2e3807c2a6ed23b25fc5d2a98c2b6c60d89e
Diane Trout <diane@caltech.edu>
2007-08-08 23:56:28 +0000

    [project @ add a basic setup.py file]

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="uashelper",
+  description="some bots and other utilities to help deal with data from an illumina sequencer",
+  author="Diane Trout",
+  author_email="diane@caltech.edu",
+  packages="uashelper",
+  scripts=[],
+)
