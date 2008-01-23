60581e340d01f974805ef5895ee43d441ebf66db
Diane Trout <diane@caltech.edu>
2008-01-23 01:52:40 +0000

    return most recent genome build for the pipeline config file.
    Brandon's original pipeline customization code replaced things
    like %(genome|build)s with the path to the ELAND genome files.
    
    What I did is made it possible to substitute keys like %(genome)s in
    addition to %(genome|build)s. The idea is that the most config files
    will be set to use whatever is the "most recent" build, but hopefully
    at some point we'll provide some way of specifying which build.
    
    The way I defined "most recent" genome build was to use the 
    alphanum sort, that sorts mixed alpha/numeric strings in the 
    'natural' order instead of ASCII order, thus "mm10" > "mm8".
    
    For the genomes that we had installed right now this would work
    for everything but arabadopsis--which appears to be using a version
    number of MMDDYYYY. Though if we changed it to YYYYMMDD everything 
    should work correctly.

---
+++ gaworkflow/pipeline/test/test_genome_mapper.py
@@ -0,0 +1,33 @@
+import unittest
+
+from StringIO import StringIO
+from gaworkflow.pipeline import genome_mapper
+
+class testGenomeMapper(unittest.TestCase):
+    def test_construct_mapper(self):
+        genomes = {
+        'Arabidopsis thaliana': {'v01212004': '/arabidopsis'},
+        'Homo sapiens': {'hg18': '/hg18'},
+        'Mus musculus': {'mm8': '/mm8',
+                        'mm9': '/mm9',
+                        'mm10': '/mm10'},
+        'Phage': {'174': '/phi'},
+        }
+        genome_map = genome_mapper.constructMapperDict(genomes)
+        
+        self.failUnlessEqual("%(Mus musculus|mm8)s" % (genome_map), "/mm8")
+        self.failUnlessEqual("%(Phage|174)s" % (genome_map), "/phi")
+        self.failUnlessEqual("%(Mus musculus)s" % (genome_map), "/mm10")
+        self.failUnlessEqual("%(Mus musculus|mm8)s" % (genome_map), "/mm8")
+        self.failUnlessEqual("%(Mus musculus|mm10)s" % (genome_map), "/mm10")
+        
+        self.failUnlessEqual(len(genome_map.keys()), 6)
+        self.failUnlessEqual(len(genome_map.values()), 6)
+        self.failUnlessEqual(len(genome_map.items()), 6)
+        
+        
+def suite():
+    return unittest.makeSuite(testGenomeMapper,'test')
+
+if __name__ == "__main__":
+    unittest.main(defaultTest="suite")
