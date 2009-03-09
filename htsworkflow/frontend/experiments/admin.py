fa8e87ded25e11d523fcd749d20494465acf77d7
Diane Trout <diane@caltech.edu>
2009-03-09 17:19:40 +0000

    Add flowcell notes field back to the admin page

--- htsworkflow/frontend/experiments/admin.py
+++ htsworkflow/frontend/experiments/admin.py
@@ -48,6 +48,7 @@ class FlowCellOptions(admin.ModelAdmin):
         ('Lanes:', {
            'fields' : (('lane_1_library', 'lane_1_pM', 'lane_1_cluster_estimate'), ('lane_2_library', 'lane_2_pM', 'lane_2_cluster_estimate'), ('lane_3_library', 'lane_3_pM', 'lane_3_cluster_estimate'), ('lane_4_library', 'lane_4_pM', 'lane_4_cluster_estimate'), ('lane_5_library', 'lane_5_pM', 'lane_5_cluster_estimate'), ('lane_6_library', 'lane_6_pM', 'lane_6_cluster_estimate'), ('lane_7_library', 'lane_7_pM', 'lane_7_cluster_estimate'), ('lane_8_library', 'lane_8_pM', 'lane_8_cluster_estimate'),)
         }),
+        ('Notes:', { 'fields': ('notes',),}),
     )
 
 class ClusterStationOptions(admin.ModelAdmin):
