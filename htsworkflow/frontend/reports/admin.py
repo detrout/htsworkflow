d883e85b2b4ed7ed2f7198402daf0b1b8e632ca0
Lorian Schaeffer <lorian@caltech.edu>
2010-10-01 01:39:37 +0000

    Removed from admin index page

--- htsworkflow/frontend/reports/admin.py
+++ htsworkflow/frontend/reports/admin.py
@@ -6,5 +6,5 @@ class ProgressReportOptions(admin.ModelAdmin):
   list_display = ('Study','ab_batch','cell_line','library','sequencing','aligned_reads','QPCR','submit_to_DCC','submit_to_NCBI','interactome_complete')
   ## list_filter = ('interactome_complete')
 
-admin.site.register(ProgressReport, ProgressReportOptions)
+#admin.site.register(ProgressReport, ProgressReportOptions)
 
