4a5ef9ae6cdfdd8cc52015b8260ca3b0dacba236
Diane Trout <diane@caltech.edu>
2012-11-19 17:09:47 -0800

    Add dependency information to the setup.py script
    Though its still missing a bit as I don't have benderjab
    hosted and librdf needs to be installed seperately.

--- setup.py
+++ setup.py
@@ -27,4 +27,14 @@ setup(
     package_data = {
         '': ['*.turtle']
         },
+    install_requires=['django >=1.1, <1.4',
+                      'lxml >= 2.2.4',
+                      'numpy >= 1.3',
+                      'librdf >= 1.0.14',
+                      'benderjab >= 0.2',
+                      'httplib2',
+                      'keyring',
+                      ],
+    include_package_data=True,
+
     )
