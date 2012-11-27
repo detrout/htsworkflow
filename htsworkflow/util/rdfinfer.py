c79ee97e6c1345658e2a48d79081258249268936
Diane Trout <diane@caltech.edu>
2012-11-27 14:22:33 -0800

    Ignoring the missing type RDF error for the page under testing's url.
    When testing the stylesheet gets attached to the pages url. All
    the meaningful information about the sample or experiment still
    gets added to the right <host>/<category>/<id> pages,
    unfortunately that means the page url doesn't have a type which
    caused _validate_types to toss an error.
    
    I'd previously fixed it by testing for the error message and
    filtering it out from the test code, but that didn't work on
    ubuntu 10.04 as the error message changes slightly with the
    older version of redland rdf.
    
    This version changes the sparql query to ignore the case
    where the predicate is a stylesheet and there's no type.

--- htsworkflow/util/rdfinfer.py
+++ htsworkflow/util/rdfinfer.py
@@ -135,12 +135,14 @@ class Infer(object):
         prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
         prefix owl: <http://www.w3.org/2002/07/owl#>
+        prefix xhtmlv: <http://www.w3.org/1999/xhtml/vocab#>
 
         select ?subject ?predicate ?object
         where {
           ?subject ?predicate ?object
           OPTIONAL { ?subject a ?class }
           FILTER(!bound(?class))
+          FILTER(?predicate != xhtmlv:stylesheet)
         }
         """
         query = RDF.SPARQLQuery(body)
