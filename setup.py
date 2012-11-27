88d68cc1c69cc43d22ee71b855f6b5cd08f312a1
Diane Trout <diane@caltech.edu>
2012-11-27 10:30:36 -0800

    Add dependencies to the setup.py
    I do need to make benderjab public before other people
    could install this. Or maybe figure out how to use the optional
    dependency mode.

--- setup.py
+++ setup.py
@@ -23,18 +23,20 @@ setup(
         "scripts/htsw-srf2fastq",
         "scripts/htsw-update-archive",
         "scripts/htsw-validate",
-        ],
+    ],
+    # I should be using one or the other package import
     package_data = {
         '': ['*.turtle']
         },
-    install_requires=['django >=1.1, <1.4',
+    include_package_data=True,
+    install_requires=['distribute',
+                      'django >=1.1, <1.4',
                       'lxml >= 2.2.4',
                       'numpy >= 1.3',
-                      'librdf >= 1.0.14',
                       'benderjab >= 0.2',
                       'httplib2',
                       'keyring',
-                      ],
-    include_package_data=True,
-
-    )
+                      # This dependency is redland librdf, which doesn't have a public egg
+                      #'librdf >= 1.0.14',
+    ],
+)
