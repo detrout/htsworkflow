9c5e5fb4495f21ae6a1fafd98d746aea3b2ad72b
Diane Trout <diane@ghic.org>
2013-10-17 14:12:05 -0700

    Implement a wrapper around bigWigInfo to report bigWig summary info.
    (shocking name there I know)

---
+++ htsworkflow/util/test/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+TEST_CODE_DIR = os.path.split(__file__)[0]
+TEST_DATA_DIR = os.path.join(TEST_CODE_DIR, 'testdata')
---
+++ htsworkflow/util/ucsc.py
@@ -0,0 +1,67 @@
+"""Wrap ucsc command line utilities
+"""
+
+import logging
+import os
+from subprocess import Popen, PIPE
+
+LOGGER = logging.getLogger(__name__)
+
+def parseNumber(number):
+    buffer = []
+    isFloat = False
+    for n in number:
+        if n == ',':
+            continue
+        if n == '.':
+            isFloat = True
+            buffer.append(n)
+        else:
+            buffer.append(n)
+    if isFloat:
+        return float(''.join(buffer))
+    else:
+        return int(''.join(buffer))
+
+def parseBoolean(value):
+    if value.lower() in ('yes', '1', 'true'):
+        return True
+    elif value.lower() in ('no', '0', 'false'):
+        return False
+        
+class bigWigInfo:
+    def __init__(self, filename=None):
+        self.version = None
+        self.isCompressed = None
+        self.isSwapped = None
+        self.primaryDataSize = None
+        self.primaryIndexSize = None
+        self.zoomLevels = None
+        self.chromCount = None
+        self.basesCovered = None
+        self.mean = None
+        self.min = None
+        self.max = None
+        self.std = None
+        self.filename = None
+        if filename:
+            self.scan_file(filename)
+            self.filename = filename
+
+    def scan_file(self, filename):
+        cmd = ['bigWigInfo', 
+               filename]
+        p = Popen(cmd, stdout=PIPE)
+        stdout, _ = p.communicate()
+        for line in stdout.split(os.linesep):
+            if len(line) > 0:
+                term, value = line.split(': ')
+                if term in ('isCompressed', 'isSwapped'):
+                    value = parseBoolean(value)
+                else:
+                    value = parseNumber(value)
+                LOGGER.debug('%s: %s', term, str(value))
+                setattr(self, term, value)
+                
+                
+
