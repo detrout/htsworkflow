ea73f3c84edaec1b55ddb088e5fcf5c060ad5477
Diane Trout <diane@ghic.org>
2015-01-29 15:44:06 -0800

    convert from failUnlessEqual to assertEqual

--- test/test_srf2fastq.py
+++ test/test_srf2fastq.py
@@ -24,19 +24,19 @@ IIIIB+++
 
         target1.seek(0)
         lines1 = target1.readlines()
-        self.failUnlessEqual(len(lines1),4)
-        self.failUnlessEqual(lines1[0].rstrip(), '@header/1')
-        self.failUnlessEqual(lines1[1].rstrip(), 'AGCT')
-        self.failUnlessEqual(lines1[2].rstrip(), '+')
-        self.failUnlessEqual(lines1[3].rstrip(), 'IIII')
+        self.assertEqual(len(lines1),4)
+        self.assertEqual(lines1[0].rstrip(), '@header/1')
+        self.assertEqual(lines1[1].rstrip(), 'AGCT')
+        self.assertEqual(lines1[2].rstrip(), '+')
+        self.assertEqual(lines1[3].rstrip(), 'IIII')
 
         target2.seek(0)
         lines2 = target2.readlines()
-        self.failUnlessEqual(len(lines2),4)
-        self.failUnlessEqual(lines2[0].rstrip(), '@header/2')
-        self.failUnlessEqual(lines2[1].rstrip(), 'TTTT')
-        self.failUnlessEqual(lines2[2].rstrip(), '+')
-        self.failUnlessEqual(lines2[3].rstrip(), 'B+++')
+        self.assertEqual(len(lines2),4)
+        self.assertEqual(lines2[0].rstrip(), '@header/2')
+        self.assertEqual(lines2[1].rstrip(), 'TTTT')
+        self.assertEqual(lines2[2].rstrip(), '+')
+        self.assertEqual(lines2[3].rstrip(), 'B+++')
 
     def test_split_at_with_header(self):
         source = StringIO("""@header1
@@ -55,19 +55,19 @@ IIIIB+++
 
         target1.seek(0)
         lines1 = target1.readlines()
-        self.failUnlessEqual(len(lines1),8)
-        self.failUnlessEqual(lines1[0].rstrip(), '@foo_header1/1')
-        self.failUnlessEqual(lines1[1].rstrip(), 'AGCT')
-        self.failUnlessEqual(lines1[2].rstrip(), '+')
-        self.failUnlessEqual(lines1[3].rstrip(), '@III')
+        self.assertEqual(len(lines1),8)
+        self.assertEqual(lines1[0].rstrip(), '@foo_header1/1')
+        self.assertEqual(lines1[1].rstrip(), 'AGCT')
+        self.assertEqual(lines1[2].rstrip(), '+')
+        self.assertEqual(lines1[3].rstrip(), '@III')
 
         target2.seek(0)
         lines2 = target2.readlines()
-        self.failUnlessEqual(len(lines2),8)
-        self.failUnlessEqual(lines2[0].rstrip(), '@foo_header1/2')
-        self.failUnlessEqual(lines2[1].rstrip(), 'TTTT')
-        self.failUnlessEqual(lines2[2].rstrip(), '+')
-        self.failUnlessEqual(lines2[3].rstrip(), 'B+++')
+        self.assertEqual(len(lines2),8)
+        self.assertEqual(lines2[0].rstrip(), '@foo_header1/2')
+        self.assertEqual(lines2[1].rstrip(), 'TTTT')
+        self.assertEqual(lines2[2].rstrip(), '+')
+        self.assertEqual(lines2[3].rstrip(), 'B+++')
 
     def test_single_at(self):
         source = StringIO("""@header1
@@ -85,11 +85,11 @@ IIIIB+++
 
         target1.seek(0)
         lines1 = target1.readlines()
-        self.failUnlessEqual(len(lines1),8)
-        self.failUnlessEqual(lines1[0].rstrip(), '@header1')
-        self.failUnlessEqual(lines1[1].rstrip(), 'AGCTTTTT')
-        self.failUnlessEqual(lines1[2].rstrip(), '+')
-        self.failUnlessEqual(lines1[3].rstrip(), '@IIIB+++')
+        self.assertEqual(len(lines1),8)
+        self.assertEqual(lines1[0].rstrip(), '@header1')
+        self.assertEqual(lines1[1].rstrip(), 'AGCTTTTT')
+        self.assertEqual(lines1[2].rstrip(), '+')
+        self.assertEqual(lines1[3].rstrip(), '@IIIB+++')
 
     def test_single_at_with_header(self):
         source = StringIO("""@header1
@@ -107,11 +107,11 @@ IIIIB+++
 
         target1.seek(0)
         lines1 = target1.readlines()
-        self.failUnlessEqual(len(lines1),8)
-        self.failUnlessEqual(lines1[0].rstrip(), '@foo_header1')
-        self.failUnlessEqual(lines1[1].rstrip(), 'AGCTTTTT')
-        self.failUnlessEqual(lines1[2].rstrip(), '+')
-        self.failUnlessEqual(lines1[3].rstrip(), '@IIIB+++')
+        self.assertEqual(len(lines1),8)
+        self.assertEqual(lines1[0].rstrip(), '@foo_header1')
+        self.assertEqual(lines1[1].rstrip(), 'AGCTTTTT')
+        self.assertEqual(lines1[2].rstrip(), '+')
+        self.assertEqual(lines1[3].rstrip(), '@IIIB+++')
 
     def test_is_srf(self):        
         cnf4_srf = 'woldlab_070829_USI-EAS44_0017_FC11055_1.srf'
@@ -120,9 +120,9 @@ IIIIB+++
         cnf1_path = os.path.join(TESTDATA_DIR, cnf1_srf)
         
         is_srf = srf2fastq.is_srf
-        self.failUnlessEqual(is_srf(__file__), False)
-        self.failUnlessEqual(is_srf(cnf4_path), True)
-        self.failUnlessEqual(is_srf(cnf1_path), True)
+        self.assertEqual(is_srf(__file__), False)
+        self.assertEqual(is_srf(cnf4_path), True)
+        self.assertEqual(is_srf(cnf1_path), True)
 
     def test_is_cnf1(self):        
         cnf4_srf = 'woldlab_070829_USI-EAS44_0017_FC11055_1.srf'
@@ -132,8 +132,8 @@ IIIIB+++
         
         is_cnf1 = srf2fastq.is_cnf1
         self.failUnlessRaises(ValueError, is_cnf1, __file__)
-        self.failUnlessEqual(is_cnf1(cnf4_path), False)
-        self.failUnlessEqual(is_cnf1(cnf1_path), True)
+        self.assertEqual(is_cnf1(cnf4_path), False)
+        self.assertEqual(is_cnf1(cnf1_path), True)
 
 
 def suite():
