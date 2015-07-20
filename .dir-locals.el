bf39e852a77033a6c68134d88f61aa7b5e1ecdab
Diane Trout <diane@ghic.org>
2015-07-20 11:35:18 -0700

    configuration file largely only useful for emacs elpy users

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (elpy-test-runner . elpy-test-django-runner)
+  (elpy-project-root . "~/proj/htsworkflow")
+  (pyvenv-workon . "htsworkflow")
+))
