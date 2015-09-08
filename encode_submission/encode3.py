c9dc3d358a038bc512c79b4a2943555cb8295656
Diane Trout <diane@caltech.edu>
2015-09-08 16:37:03 -0700

    convert from optparse to argparse

--- encode_submission/encode3.py
+++ encode_submission/encode3.py
@@ -3,13 +3,12 @@
 """
 from __future__ import print_function, unicode_literals
 
-from ConfigParser import SafeConfigParser
+import argparse
 import fnmatch
 from glob import glob
 import json
 import logging
 import netrc
-from optparse import OptionParser, OptionGroup
 import os
 from pprint import pprint, pformat
 import shlex
@@ -46,29 +45,29 @@ if not 'DJANGO_SETTINGS_MODULE' in os.environ:
 
 def main(cmdline=None):
     parser = make_parser()
-    opts, args = parser.parse_args(cmdline)
+    args = parser.parse_args(cmdline)
     submission_uri = None
 
     from django.conf import settings
 
-    if opts.debug:
+    if args.debug:
         settings.LOGGING['loggers']['htsworkflow']['level'] = 'DEBUG'
-    elif opts.verbose:
+    elif args.verbose:
         settings.LOGGING['loggers']['htsworkflow']['level'] = 'INFO'
 
     django.setup()
 
-    model = get_model(opts.model, opts.db_path)
+    model = get_model(args.model, args.db_path)
     submission_names = list(list_submissions(model))
-    name = opts.name
-    if len(submission_names) == 0 and opts.name is None:
+    name = args.name
+    if len(submission_names) == 0 and args.name is None:
         parser.error("Please name this submission")
-    elif opts.name and submission_names and opts.name not in submission_names:
+    elif args.name and submission_names and args.name not in submission_names:
         parser.error("{} is not in this model. Choose from: {}{}".format(
-            opts.name,
+            args.name,
             os.linesep,
             INDENTED.join(submission_names)))
-    elif opts.name is None and len(submission_names) > 1:
+    elif args.name is None and len(submission_names) > 1:
         parser.error("Please choose submission name from: {}{}".format(
             os.linesep,
             INDENTED.join(submission_names)))
@@ -79,101 +78,103 @@ def main(cmdline=None):
         submission_uri = get_submission_uri(name)
         logger.info('Submission URI: %s', submission_uri)
 
-    mapper = AWSSubmission(name, model, encode_host=opts.encoded, lims_host=opts.host)
+    mapper = AWSSubmission(name, model, encode_host=args.encoded, lims_host=args.host)
 
-    if opts.load_rdf is not None:
+    if args.load_rdf is not None:
         if submission_uri is None:
             parser.error("Please specify the submission name")
-        load_into_model(model, 'turtle', opts.load_rdf, submission_uri)
+        load_into_model(model, 'turtle', args.load_rdf, submission_uri)
 
     results = ResultMap()
-    for a in args:
+    for a in args.libraries:
         if os.path.exists(a):
             results.add_results_from_file(a)
         else:
             logger.warn("File %s doesn't exist.", a)
 
-    if opts.make_link_tree_from is not None:
-        results.make_tree_from(opts.make_link_tree_from, link=True)
+    if args.make_link_tree_from is not None:
+        results.make_tree_from(args.make_link_tree_from, link=True)
 
-    if opts.copy_tree_from is not None:
-        results.make_tree_from(opts.copy_tree_from, link=False)
+    if args.copy_tree_from is not None:
+        results.make_tree_from(args.copy_tree_from, link=False)
 
-    if opts.fastq:
+    if args.fastq:
         logger.info("Building fastq extraction scripts")
-        flowcells = os.path.join(opts.sequence, 'flowcells')
-        extractor = CondorFastqExtract(opts.host, flowcells,
-                                       model=opts.model,
-                                       compression=opts.compression,
-                                       force=opts.force)
+        flowcells = os.path.join(args.sequence, 'flowcells')
+        extractor = CondorFastqExtract(args.host, flowcells,
+                                       model=args.model,
+                                       compression=args.compression,
+                                       force=args.force)
         extractor.create_scripts(results)
 
-    if opts.scan_submission:
+    if args.scan_submission:
         if name is None:
             parser.error("Please define a submission name")
         mapper.scan_submission_dirs(results)
 
-    if opts.upload:
-        mapper.upload(results, opts.dry_run)
+    if args.upload:
+        mapper.upload(results, args.dry_run)
 
-    if opts.sparql:
-        sparql_query(model, opts.sparql)
+    if args.sparql:
+        sparql_query(model, args.sparql)
 
-    if opts.print_rdf:
+    if args.print_rdf:
         writer = get_serializer()
         print(writer.serialize_model_to_string(model))
 
 
 def make_parser():
-    parser = OptionParser()
+    parser = argparse.ArgumentParser()
 
-    model = OptionGroup(parser, 'model')
-    model.add_option('--name', help="Set submission name")
-    model.add_option('--db-path', default=None,
+    model = parser.add_argument_group('model')
+    model.add_argument('--name', help="Set submission name")
+    model.add_argument('--db-path', default=None,
                      help="set rdf database path")
-    model.add_option('--model', default=None,
+    model.add_argument('--model', default=None,
                      help="Load model database")
-    model.add_option('--load-rdf', default=None,
+    model.add_argument('--load-rdf', default=None,
                      help="load rdf statements into model")
-    model.add_option('--sparql', default=None, help="execute sparql query")
-    model.add_option('--print-rdf', action="store_true", default=False,
+    model.add_argument('--sparql', default=None, help="execute sparql query")
+    model.add_argument('--print-rdf', action="store_true", default=False,
                      help="print ending model state")
-    parser.add_option_group(model)
+
     # commands
-    commands = OptionGroup(parser, 'commands')
-    commands.add_option('--make-link-tree-from',
+    commands = parser.add_argument_group('commands')
+    commands.add_argument('--make-link-tree-from',
                         help="create directories & link data files",
                         default=None)
-    commands.add_option('--copy-tree-from',
+    commands.add_argument('--copy-tree-from',
                         help="create directories & copy data files",
                         default=None)
-    commands.add_option('--fastq', default=False, action="store_true",
+    commands.add_argument('--fastq', default=False, action="store_true",
                         help="generate scripts for making fastq files")
-    commands.add_option('--scan-submission', default=False, action="store_true",
+    commands.add_argument('--scan-submission', default=False, action="store_true",
                         help="cache md5 sums")
-    commands.add_option('--upload', default=False, action="store_true",
+    commands.add_argument('--upload', default=False, action="store_true",
                         help="Upload files")
 
-    parser.add_option_group(commands)
-
-    parser.add_option('--force', default=False, action="store_true",
+    parser.add_argument('--force', default=False, action="store_true",
                       help="Force regenerating fastqs")
-    parser.add_option('--compression', default=None, type='choice',
+    parser.add_argument('--compression', default=None,
                       choices=['gzip'],
                       help='select compression type for fastq files')
-    parser.add_option('--library-url', default=None,
+    parser.add_argument('--library-url', default=None,
                       help="specify an alternate source for library information")
-    parser.add_option('--encoded', default='www.encodeproject.org',
+    parser.add_argument('--encoded', default='www.encodeproject.org',
                       help='base url for talking to encode server')
-    parser.add_option('--dry-run', default=False, action='store_true',
+    parser.add_argument('--dry-run', default=False, action='store_true',
                       help='avoid making changes to encoded')
+    
     # debugging
-    parser.add_option('--verbose', default=False, action="store_true",
+    parser.add_argument('--verbose', default=False, action="store_true",
                       help='verbose logging')
-    parser.add_option('--debug', default=False, action="store_true",
+    parser.add_argument('--debug', default=False, action="store_true",
                       help='debug logging')
 
     api.add_auth_options(parser)
+     
+    parser.add_argument('libraries', nargs='+',
+                        help='mapping of library id to directory to be processed')
 
     return parser
 
