f3a59e81521189a4389488d92ab49cef31bbf3a0
Diane Trout <diane@ghic.org>
2022-10-07 15:37:17 -0700

    Python3.6 deprecates putting regular expression flags at the end of a string

--- experiments/migrations/0002_load_filedata.py
+++ experiments/migrations/0002_load_filedata.py
@@ -5,25 +5,25 @@ from django.db import models, migrations
 
 def initialize_file_type(apps, migrations):
     data = [
-        ('run_xml', 'application/vnd.htsworkflow-run-xml', 'run.*\\.xml\\Z(?ms)'),
-        ('Summary.htm', 'text/html', 'Summary\\.htm\\Z(?ms)'),
-        ('IVC All', 'image/png', 's_(?P<lane>[0-9])_all\\.png\\Z(?ms)'),
-        ('IVC Call', 'image/png', 's_(?P<lane>[0-9])_call\\.png\\Z(?ms)'),
-        ('IVC Percent All', 'image/png', 's_(?P<lane>[0-9])_percent_all\\.png\\Z(?ms)'),
-        ('IVC Percent Base', 'image/png', 's_(?P<lane>[0-9])_percent_base\\.png\\Z(?ms)'),
-        ('IVC Percent Call', 'image/png', 's_(?P<lane>[0-9])_percent_call\\.png\\Z(?ms)'),
-        ('GERALD Scores', None, 's_(?P<lane>[0-9])_percent_call\\.png\\Z(?ms)'),
+        ('run_xml', 'application/vnd.htsworkflow-run-xml', '(?ms)run.*\\.xml\\Z'),
+        ('Summary.htm', 'text/html', '(?ms)Summary\\.htm\\Z'),
+        ('IVC All', 'image/png', '(?ms)s_(?P<lane>[0-9])_all\\.png\\Z'),
+        ('IVC Call', 'image/png', '(?ms)s_(?P<lane>[0-9])_call\\.png\\Z'),
+        ('IVC Percent All', 'image/png', '(?ms)s_(?P<lane>[0-9])_percent_all\\.png\\Z'),
+        ('IVC Percent Base', 'image/png', '(?ms)s_(?P<lane>[0-9])_percent_base\\.png\\Z'),
+        ('IVC Percent Call', 'image/png', '(?ms)s_(?P<lane>[0-9])_percent_call\\.png\\Z'),
+        ('GERALD Scores', None, '(?ms)s_(?P<lane>[0-9])_percent_call\\.png\\Z'),
         ('ELAND Result', 'chemical/seq-na-eland',
-         's_(?P<lane>[0-9])((?P<end>[1-4])_)?_eland_result\\.txt\\.bz2\\Z(?ms)'),
+         '(?ms)s_(?P<lane>[0-9])((?P<end>[1-4])_)?_eland_result\\.txt\\.bz2\\Z'),
         ('ELAND Multi', 'chemical/seq-na/eland',
-         's_(?P<lane>[0-9])((?P<end>[1-4])_)?_eland_multi\\.txt\\.bz2\\Z(?ms)'),
+         '(?ms)s_(?P<lane>[0-9])((?P<end>[1-4])_)?_eland_multi\\.txt\\.bz2\\Z'),
         ('ELAND Extended', 'chemical/seq-na-eland',
-         's_(?P<lane>[0-9])((?P<end>[1-4])_)?_eland_extended\\.txt\\.bz2\\Z(?ms)'),
+         '(?ms)s_(?P<lane>[0-9])((?P<end>[1-4])_)?_eland_extended\\.txt\\.bz2\\Z'),
         ('ELAND Export', 'chemical/seq-na-eland',
-         's_(?P<lane>[0-9])((?P<end>[1-4])_)?_export\\.txt\\.bz2\\Z(?ms)'),
-        ('SRF', 'chemical/seq-na/eland', '.*_(?P<lane>[1-8])\\.srf\\Z(?ms)'),
+         '(?ms)s_(?P<lane>[0-9])((?P<end>[1-4])_)?_export\\.txt\\.bz2\\Z'),
+        ('SRF', 'chemical/seq-na/eland', '(?ms).*_(?P<lane>[1-8])\\.srf\\Z'),
         ('QSEQ tarfile', 'chemical/seq-na-qseq',
-         '.*_l(?P<lane>[1-8])_r(?P<end>[1-4])\\.tar\\.bz2\\Z(?ms)'),
+         '(?ms).*_l(?P<lane>[1-8])_r(?P<end>[1-4])\\.tar\\.bz2\\Z'),
         ('HiSeq Fastq', 'chemical/seq-na-fastq',
          '(?P<library>[0-9]+)_(NoIndex|[AGCT]+)_L(?P<lane>[0-9]+)_R(?P<end>[12])_(?P<split>[0-9]+)\\.fastq\\.gz'),
 
@@ -35,7 +35,7 @@ def initialize_file_type(apps, migrations):
                   'regex': regex}
         FileType = apps.get_model('experiments', 'FileType')
         o = FileType.objects.get_or_create(**record)
-        
+
 
 class Migration(migrations.Migration):
 
