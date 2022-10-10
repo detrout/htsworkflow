b880fa8b7a17df64d9c1ee8e0c3f3c7767be74f6
Diane Trout <diane@ghic.org>
2022-10-10 12:53:05 -0700

    Convert from django.conf.urls.url to django.urls.path
    Well there's one place I had to use re_path instead of path,
    but I did move away from the original conf.urls.

--- htsworkflow/urls.py
+++ htsworkflow/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include,  url
+from django.urls import include, path
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,23 +13,25 @@ from experiments.views import (
 from samples.views import library_id_to_admin_url
 
 urlpatterns = [
-    url('^accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
     # Base:
-    url(r'^eland_config/', include('eland_config.urls')),
+    path("eland_config/", include("eland_config.urls")),
     # Experiments:
-    url(r'^experiments/', include('experiments.urls')),
-    url(r'^lane/(?P<lane_pk>\w+)',
-        flowcell_lane_detail, name="flowcell_lane_detail"),
-    url(r'^flowcell/(?P<flowcell_id>\w+)/((?P<lane_number>\w+)/)?$',
-        flowcell_detail, name="flowcell_detail"),
-    url(r'^flowcell/$', FlowCellListView.as_view(), name='flowcell_index'),
-    url(r'^inventory/', include('inventory.urls')),
-    url(r'^library/', include('samples.urls')),
-    url(r'^lanes_for/(?P<username>[-_ \w]+)?',
-        lanes_for, name='lanes_for'),
+    path("experiments/", include("experiments.urls")),
+    path("lane/<lane_pk>", flowcell_lane_detail, name="flowcell_lane_detail"),
+    path("flowcell/<flowcell_id>/", flowcell_detail, name="flowcell_detail"),
+    path(
+        "flowcell/<flowcell_id>/<lane_number>/",
+        flowcell_detail,
+        name="flowcell_detail",
+    ),
+    path("flowcell/", FlowCellListView.as_view(), name="flowcell_index"),
+    path("inventory/", include("inventory.urls")),
+    path("library/", include("samples.urls")),
+    path("lanes_for/", lanes_for, name="lanes_for"),
+    path("lanes_for/<username>", lanes_for, name="lanes_for"),
     ### library id to admin url
-    url(r'^library_id_to_admin_url/(?P<lib_id>\w+)/$', library_id_to_admin_url),
-    url(r'^sequencer/(?P<sequencer_id>\w+)', sequencer, name="sequencer"),
-
-    url(r'^admin/', admin.site.urls),
+    path("library_id_to_admin_url/<library_id>/", library_id_to_admin_url),
+    path("sequencer/<sequencer_id>", sequencer, name="sequencer"),
+    path("admin/", admin.site.urls),
 ]
